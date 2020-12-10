1f3e362bebd5fa81317c5019cff31f23b4e1990d
Andrew Lyons <aslyons001@gmail.com>
2020-12-09 20:39:36 -0500

    Updates

---
+++ precache-manifest.ca471e0ceba0dca88695081f8965139d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a2787d262d6cda25239b99367a34e50",
+    "url": "/rcg-landing-page2/index.html"
+  },
+  {
+    "revision": "6fb27a23e5d3e6fb806f",
+    "url": "/rcg-landing-page2/static/css/2.888105a8.chunk.css"
+  },
+  {
+    "revision": "6429efd8c7f159a8010c",
+    "url": "/rcg-landing-page2/static/css/main.dbae2d7d.chunk.css"
+  },
+  {
+    "revision": "6fb27a23e5d3e6fb806f",
+    "url": "/rcg-landing-page2/static/js/2.8ff8d825.chunk.js"
+  },
+  {
+    "revision": "451ffe720aa8add9f554b8ed54913322",
+    "url": "/rcg-landing-page2/static/js/2.8ff8d825.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6429efd8c7f159a8010c",
+    "url": "/rcg-landing-page2/static/js/main.d92ca0a0.chunk.js"
+  },
+  {
+    "revision": "b613487fec594158e49a",
+    "url": "/rcg-landing-page2/static/js/runtime-main.335e2938.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/rcg-landing-page2/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/rcg-landing-page2/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/rcg-landing-page2/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/rcg-landing-page2/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/rcg-landing-page2/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "bd09b2abb9736358f4e8882727e55ea5",
+    "url": "/rcg-landing-page2/static/media/rcg-logo-v4.bd09b2ab.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rcg-landing-page/precache-manifest.601738754c943021d1f8f7071aea5bff.js"
+  "/rcg-landing-page2/precache-manifest.ca471e0ceba0dca88695081f8965139d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rcg-landing-page/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rcg-landing-page2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
